7dfb6250b0e0b4f9abc8c0fec581bc8a21c08954
Sho Hashimoto <hashimoto@shokai.org>
2016-09-22 08:21:22 +0900

    fixed sample

--- sample/source.js
+++ sample/source.js
@@ -1,6 +1,7 @@
 /* eslint-env browser */
 
 var execCopy = require('../')
+// var execCopy = require('exec-copy')
 
 window.addEventListener('load', function (e) {
   var btn = document.getElementById('btnCopy')
