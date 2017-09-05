6ea123e7179ab8bf07fcae2359798f773c994009
Sho Hashimoto <hashimoto@shokai.org>
2017-09-05 17:39:35 +0900

    always use textarea

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 export default function copy (str, onError) {
   if (typeof str !== 'string') return
   if (!window || !document) return
-  const input = (str.includes('\n'))
-  ? document.createElement('textarea') : document.createElement('input')
-  document.body.appendChild(input)
-  input.setAttribute('readonly', true)
-  input.style.position = 'absolute'
-  input.style.left = '-1000px'
-  input.style.top = document.body.scrollTop + 'px'
-  input.value = str
-  input.focus()
-  input.setSelectionRange(0, str.length)
+  const textarea = document.createElement('textarea')
+  document.body.appendChild(textarea)
+  textarea.setAttribute('readonly', true)
+  textarea.style.position = 'absolute'
+  textarea.style.left = '-1000px'
+  textarea.style.top = document.body.scrollTop + 'px'
+  textarea.value = str
+  textarea.focus()
+  textarea.setSelectionRange(0, str.length)
   const result =
           typeof document.execCommand === 'function' &&
           document.execCommand('copy')
-  document.body.removeChild(input)
+  document.body.removeChild(textarea)
   if (!result) {
     if (typeof onError === 'function') onError(str)
     else window.prompt('Copy', str) // fallback
