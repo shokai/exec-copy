fa984f5b4d4d45523b2dcfc7c023e2b8cea25b1c
Sho Hashimoto <hashimoto@shokai.org>
2022-01-18 18:35:02 +0900

    use var instead of const

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function copy(str, onError) {
   if (typeof str !== 'string') return
   if (!window || !document) return
-  const textarea = document.createElement('textarea')
+  var textarea = document.createElement('textarea')
   document.body.appendChild(textarea)
   textarea.setAttribute('readonly', true)
   textarea.style.position = 'absolute'
@@ -10,7 +10,7 @@ module.exports = function copy(str, onError) {
   textarea.value = str
   textarea.focus()
   textarea.setSelectionRange(0, str.length)
-  const result =
+  var result =
     typeof document.execCommand === 'function' && document.execCommand('copy')
   document.body.removeChild(textarea)
   if (!result) {
